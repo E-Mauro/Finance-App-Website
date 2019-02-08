84eb503ce381f394b7180908ce37a7953f813dc2
Juan Areaga <kerusso1984@gmail.com>
2019-02-08 10:22:42 -0600

    Format current document

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -2,26 +2,26 @@ import React, { Component } from 'react';
 
 
 class NavigationBar extends Component {
-  render() {
-    return (
-      <main>
-<header>
-		<div class="menu_bar">
-			<a href="#" class="bt-menu"><span class="icon-list2"></span>Finance-App</a>
-		</div>
- 
-		<nav>
-			<ul>
-				<li><a href="#"><span class="icon-house"></span>Inicio</a></li>
-				<li><a href="#"><span class="icon-suitcase"></span>Manual</a></li>
-				<li><a href="#"><span class="icon-earth"></span>Contacto</a></li>
-				<li><a href="#"><span class="icon-mail"></span>Acerca de..</a></li>
-			</ul>
-		</nav>
-	</header>
-      </main>
-    );
-  }
+	render() {
+		return (
+			<main>
+				<header>
+					<div class="menu_bar">
+						<a href="#" class="bt-menu"><span class="icon-list2"></span>Finance-App</a>
+					</div>
+
+					<nav>
+						<ul>
+							<li><a href="#"><span class="icon-house"></span>Inicio</a></li>
+							<li><a href="#"><span class="icon-suitcase"></span>Manual</a></li>
+							<li><a href="#"><span class="icon-earth"></span>Contacto</a></li>
+							<li><a href="#"><span class="icon-mail"></span>Acerca de..</a></li>
+						</ul>
+					</nav>
+				</header>
+			</main>
+		);
+	}
 }
 
 export default NavigationBar;
