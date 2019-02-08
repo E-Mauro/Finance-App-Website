d51a3de9881d41f2b6e575749fd01b281a0e59b9
E-mauro <edmundo-01@outlook.com>
2019-02-08 11:57:33 -0600

    Rediseña estilos para mejorar el rendimiento

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -5,18 +5,23 @@ import "./HomePage.css"
 class HomePage extends Component {
     render() {
         return (
-            <div className="flex-container">
+            <div class="flex-container">
                 <div>
                     <div>
                         <h1>Super cartera virtual</h1>
-                        <img alt="logo" src={logo} />
+                        <div className="App_Icon">
+                            <img src="https://lh3.googleusercontent.com/LBIyqZbsxXWWQWjsWD5moPJhQQ5L0OKUFNNEbNfUAyJR8vBhPTNll4m2Z2-Lu6reuU4=s180-rw" alt="App Icon" />
+                        </div>
+                        <br />
                         <h3>¿A donde fue tu dinero?</h3>
                         <p>Comienza a organizar tus finanzas y descubrelo</p>
                     </div>
+                    <br />
                     <div>
                         <h3>Facil de usar !</h3>
                         <p>Registra tus nuevos gastos en menos de 10 segundos</p>
                     </div>
+                    <br />
                     <div>
                         <button>Descargar para Android</button>
                         <button>Descargar para IOS</button>
